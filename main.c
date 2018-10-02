01071ab5ab530d82b71ae8188126eaff9f975556
Waffle <superotherguy@gmail.com>
2018-10-02 16:51:29 -0400

    Binary String input and output
    Generated file outputs in Binary String format, Converter parses from Binary String format.
    Needs support for more variable flight data lengths.

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /*
 Authors:
-    John Lahut
-    James Bohrer
-    Jason Deacutis 
+	John Lahut
+	James Bohrer
+	Jason Deacutis
 Date: 9.30.2018
 Filename: utils.c
 Purpose: Main entry point of program.
@@ -13,16 +13,21 @@ Project: CSI402 Final Project
 
 #include "header.h"
 
+
 int main(int argc, char* argv[]) {
+	//const int num_flights = 2;// insisted on constant but wouldn't compile
+	#define num_flights 2
 
-    int num_flights = 2;
+	Flight flights[num_flights];
 
-    Flight flights[num_flights];
+	generate_file("output.txt", num_flights);
+	//convert("output.bin", "", flights);
+	convertBinaryStringFile("output.bin", flights, num_flights);
 
-    generate_file("output.txt", num_flights);
-    convert("output.bin", "", flights);
+	for (int i = 0; i < num_flights; i++) {
+		print_flight(flights[i]);
+	}
 
-    for (int i=0; i<num_flights; i++){
-        print_flight(flights[i]);
-    }
+	//printf("\n\nStrToBin: %s\n", strToBinStr("Test"));
+	//printf("BinToStr: %s\n", binStrToStr(strToBin("Test")));
 }
--- utils.c
+++ utils.c
@@ -1,8 +1,8 @@
 /*
 Authors:
-    John Lahut
-    James Bohrer
-    Jason Deacutis 
+	John Lahut
+	James Bohrer
+	Jason Deacutis
 Date: 9.30.2018
 Filename: utils.c
 Purpose: General purpose utility functions that may be used across programs.
@@ -14,10 +14,59 @@ Project: CSI402 Final Project
 #include "header.h"
 
 int random_range(int lower, int upper) {
-    return (rand() % (upper - lower + 1)) + lower;
+	return (rand() % (upper - lower + 1)) + lower;
 }
 
 void print_flight(Flight flight) {
-    printf("Flight Code: %s | Origin Airport: %s | Destination Airport: %s | Departure Date: %s\n",
-        flight.f_code, flight.origin, flight.dest, flight.timestamp);
+	printf("Flight Code: %s | Origin Airport: %s | Destination Airport: %s | Departure Date: %s\n",
+		flight.f_code, flight.origin, flight.dest, flight.timestamp);
+}
+
+char* strToBinStr(char *s) {
+	int len = strlen(s), binLen = len * CHAR_BIT;
+	char *bin = malloc(binLen + 1);
+	if (bin == NULL) {
+		printf("strToBin(%s) malloc fail\n", s);
+		return NULL;
+	}
+
+	int k = 0;
+	for (int i = 0; i <= len; i++) {
+		char c = s[i];
+
+		for (int j = CHAR_BIT - 1; j >= 0; j--) {
+			bin[k++] = ((c >> j) & 1) == 1 ? '1' : '0';
+		}
+	}
+
+	bin[binLen] = '\0';
+	return bin;
+}
+
+char* binStrToStr(char *bin) {
+	int len = strlen(bin), strLen = len / CHAR_BIT;
+
+	char *str = malloc(strLen + 1);
+	if (bin == NULL) {
+		printf("binToStr(%s) malloc fail\n", bin);
+		return NULL;
+	}
+
+	int k = 0;
+	for (int i = 0; i <= strLen; i++) {
+		char bitc = bin[i];
+		int bit = bitc == '1' ? 1 : 0;
+
+		char c = 0;
+
+		for (int j = CHAR_BIT - 1; j >= 0; j--) {
+			if (bin[k++] == '1')  c |= 1 << j;
+			else c &= ~(1 << j);
+		}
+
+		str[i] = c;
+	}
+
+	str[strLen] = '\0';
+	return str;
 }
